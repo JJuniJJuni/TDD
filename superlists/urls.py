72e37861614f1c7aaf7af19a28103ca79ce22585
gud305 <hyoungjun.park@rgpkorea.co.kr>
2018-03-28 10:28:59 +0900

    make test method checking if views method exists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import include, url
 
 urlpatterns = [
     # Examples:
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'lists.views.home_page', name='home'),
 ]
