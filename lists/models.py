e58ea991dccecd761cfe94012aa09785c596c8bc
gud305 <hyoungjun.park@rgpkorea.co.kr>
2018-04-02 17:42:34 +0900

    make model List

--- lists/models.py
+++ lists/models.py
@@ -3,7 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
-    text = models.TextField(default="")
+    text = models.TextField(default=" ")
     # 새로운 필드(Column)을 생성 할 때 마다 'makemigration' 실행
     # 만약 매개변수로 default 값 안주면, 줄거냐고 커맨드 창에서 물어봄
+    list = models.ForeignKey(List, default=None)
+
+
--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -60,15 +60,23 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        # list 생성한 다음에 데이터베이스에 저장이 잘 되있는지 확인
 
         saved_items = Item.objects.all()
         # Django는 디폴트로 모든 모델 클래스에 대해 'objects'라는 Manager 객체를
@@ -80,7 +88,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -89,8 +99,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemy 1')
-        Item.objects.create(text='itemy 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemy 1', list=list_)
+        Item.objects.create(text='itemy 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
         # GET 요청(get(url) 메소드)을 해당 url에서 시뮬레이트 해본다.
